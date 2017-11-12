cdafc81325507aa472c698e7f98b5ca966fffed5
Junghoon Lee <scarlet3628@likelion.org>
2017-11-12 23:41:06 +0000

    1st

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def index
+  end
+  
+  def form
+  end
+  
+  def cityjob
+  end
+  
+  def factory
+  end
+  
+  def farm
+  end
+  
+  def etc
+  end
+  
+  def postinpost
+  end
+end
