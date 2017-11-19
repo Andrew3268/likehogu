dd4d74c4ceccaa4bd06fdf6af221d7457f151960
이정훈 <junghoonlee@192-168-1-9.tpgi.com.au>
2017-11-19 20:09:35 +1000

    index&guideline

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/home/farm'
   get '/home/etc'
   get '/home/postinpost'
+  get '/home/information'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
